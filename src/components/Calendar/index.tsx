bca73b2fea3d9217f48b160d5c184ab3ce792801
Bruna <brunaporato@gmail.com>
2024-01-08 09:09:07 -0300

    feat: months navigation

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -9,22 +9,41 @@ import {
 } from './styles'
 import { getWeekDays } from '@/utils/get-week-days'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
-  const [] = useState(new Date())
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const nextMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(nextMonthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Janeiro <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarAction>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarAction>
